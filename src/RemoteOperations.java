dec6bbb1f9fcebe3b0d271aecefd17693520d659
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2024-10-24 15:59:31 -0400

    Defined remote interface for use of RPC

---
+++ src/RemoteOperations.java
@@ -0,0 +1,12 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Remote Interface which declares the necessary remote methods
+ *  for use with the Remote Procedure Call functionality of the client/server program.
+ */
+public interface RemoteOperations extends Remote {
+    String createRecord() throws RemoteException;
+    String getRecord() throws RemoteException;
+    String deleteRecord() throws RemoteException;
+}
